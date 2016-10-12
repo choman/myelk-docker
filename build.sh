feb2f82818e1f23c1af1ed7046235c2a2723231d
Chad Homan <choman@gmail.com>
2016-10-12 14:34:43 -0400

    Minor updates

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-cd elk-docker
-docker build -t elk .
+
+which docker-compose > /dev/null
+rval=$?
+
+if [ $rval -eq 1 ]; then
+    echo using: docker
+    cd elk-docker
+    docker build -t elk .
+else
+    echo using: docker-compose
+    docker-compose build
+fi
--- runme.sh
+++ runme.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -it --name elk elk
+
+which docker-compose > /dev/null
+rval=$?
+
+if [ $rval -eq 1 ]; then
+    echo using: docker
+    docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -it --name elk elk
+
+else
+    echo using: docker-compose
+    docker-compose start
+fi
