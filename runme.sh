5f8caeac1e736250e76fe84a0de5ae4ec9085ee8
Chad Homan <choman@gmail.com>
2016-10-12 16:46:58 -0400

    Added -d and --no-recreate

--- runme.sh
+++ runme.sh
@@ -6,9 +6,11 @@ rval=$?
 
 if [ $rval -eq 1 ]; then
     echo using: docker
-    docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -p 5000:5000 -it --name elk elk
+    docker run --rm --restart yes -p 5601:5601 -p 9200:9200 \
+                                  -p 5044:5044 -p 5000:5000 -it --name elk elk
 
 else
     echo using: docker-compose
-    docker-compose up
+    docker-compose up --no-recreate -d
 fi
+
